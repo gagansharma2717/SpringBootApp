81306e6e7975c2ca22f3c61984cb9fdb6eae2a41
Gagan Sharma <gagansharma2717@gmail.com>
2022-03-26 17:16:11 +0530

    Get and Delete Departments

--- src/main/java/com/sampleapp/SpringBootApp/tutorial/service/DepartmentService.java
+++ src/main/java/com/sampleapp/SpringBootApp/tutorial/service/DepartmentService.java
@@ -8,4 +8,8 @@ public interface DepartmentService {
     public Department saveDepartment(Department department);
 
     public List<Department> fetchDepartmentList();
+
+    public Department fetchdepartmentByID(Long departmentId);
+
+    public void deleteDepartmentById(Long departmentId);
 }
