ac303b06b0302d87f25c6d32ddc40500b9e694af
Gagan Sharma <gagansharma2717@gmail.com>
2022-03-31 18:10:37 +0530

    Using Values from application.properties

--- src/main/java/com/sampleapp/SpringBootApp/tutorial/Controller/HelloController.java
+++ src/main/java/com/sampleapp/SpringBootApp/tutorial/Controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.sampleapp.SpringBootApp.tutorial.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${Welcome.Message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String HelloWorld(){
-            return "Hello Gagan Sharma !!!";
+            return welcomeMessage;
     }
 }
